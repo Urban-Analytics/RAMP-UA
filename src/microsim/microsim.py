28d9fbd56dcc49aae1572a417f2306f51f86b903
Nick Malleson <nick@geo-mac78.broadband>
2020-05-08 05:37:41 +0100

    added .gitignore

--- src/microsim/microsim.py
+++ src/microsim/microsim.py
@@ -11,20 +11,69 @@ Created on Wed Apr 29 19:59:25 2020
 import pandas as pd
 import glob
 import os
+import random
+import time
+import typing
 
 import click # command-line interface
 
 DATA_DIR = "../../data/"
 
 class Microsim:
-    
-    def __init__(self):
-        self.households, self.individuals = Microsim._read_msm_data()
+    """
+    A class used to represent the microsimulation model.
+
+    TODO: Document class. Please use reStructuredText format:
+    https://www.sphinx-doc.org/en/master/usage/restructuredtext/basics.html
+    ...
+
+    Attributes
+    ----------
+    XXXX
+
+    Methods
+    -------
+    XXXX
+    """
+
+    def __init__(self, random_seed:float = None):
+        """
+        Microsim constructor.
+        ----------
+        :param random_seed: A optional random seed to use when creating the class instance. If None then
+            the current time is used.
+        """
+
+        # This is the main population of individuals and their households
+        self.individuals, self.households = Microsim.read_msm_data()
+
+        # Attach a load of health attributes to each individual
+        self.individuals = Microsim.attach_health_data(self.individuals)
+
+        # Attach a load of transport attributes to each individual
+        self.individuals = Microsim.attach_transport_data(self.individuals)
+
+        # Read the locations of schools, workplaces, etc.
+        #self.schools = Microsim.read_school_data()
+        #self.shops = Microsim.read_shop_data()
+        #self.workplaces = Microsim.read_workplace_data()
+
+        # Assign probabilities that each individual will go to each location (most of these will be 0!)
+        # Do this by adding two new columns, both storing lists. One lists ids of the locations that
+        # the individual may visit, the other has the probability of them visitting those places
+
+
+
+        # Other variables that need to be defined
         self.iteration = 0
+        self.random = random.Random(time.time() if random_seed is None else random_seed)
     
-    @classmethod
-    def _read_msm_data(cls):
-        """Read the csv files that have the indivduls and households"""
+    @staticmethod
+    def read_msm_data():
+        """Read the csv files that have the indivduls and households
+
+        :return a tuple with two pandas dataframes representing individuls (0) and households (1)
+        """
         
         msm_dir = os.path.join(DATA_DIR,"msm_data")
     
@@ -58,10 +107,31 @@ class Microsim:
               f"households in {len(households.Area.unique())} areas",
               f"\n\tIndividuals: {len(indiv_dfs)} files with {len(individuals)}",
               f"individuals in {len(individuals.Area.unique())} areas")
-        
-        return (households, individuals)
-    
-    
+
+        # TODO Join individuls to households (create lookup columns in each)
+
+        return (individuals, households)
+
+    @staticmethod
+    def attach_health_data(individuals):
+        """
+
+        :param individuals:
+        :return:
+        """
+        print("Attaching health data ... ",)
+        pass
+        print("... finished.")
+        return individuals
+
+    @staticmethod
+    def attach_transport_data(individuals):
+        print("Attaching health data ... ",)
+        pass
+        print("... finished.")
+        return individuals
+
+
     def step(self):
         """Step (iterate) the model"""
         self.iteration +=1 
@@ -81,6 +151,8 @@ def run(iterations):
     m = Microsim()
     for i in range(num_iter):
         m.step()
+    print("End of program")
 
 if __name__=="__main__":
     run()
+    print("End of program")
