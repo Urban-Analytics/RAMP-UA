5eb739eb7a95412959f6d937fcdf609c93c8573f
Nick Malleson <nick@geo-mac78.broadband>
2020-05-10 13:31:37 +0100

    Read in Marks retail data. Now just need to link that to individuals

--- src/microsim/microsim.py
+++ src/microsim/microsim.py
@@ -72,24 +72,25 @@ class Microsim:
 
         # Read the locations of schools, workplaces, etc.
         # self.schools = Microsim.read_school_data()
-        self.shops = Microsim.read_shop_data()
+        self.stores, self.stores_flows = Microsim.read_retail_flows_data()  # (list of shops and a flow matrix)
         # self.workplaces = Microsim.read_workplace_data()
 
         # Assign probabilities that each individual will go to each location (most of these will be 0!)
         # Do this by adding two new columns, both storing lists. One lists ids of the locations that
         # the individual may visit, the other has the probability of them visitting those places
+        self.individuals = Microsim.add_individual_flows("Retail", self.individuals, self.stores_flows)
 
         print(".. End of __init__() .. ")
         return
 
-    @staticmethod
-    def read_msm_data():
+    @classmethod
+    def read_msm_data(cls) -> (pd.DataFrame, pd.DataFrame):
         """Read the csv files that have the indivduls and households
 
         :return a tuple with two pandas dataframes representing individuls (0) and households (1)
         """
 
-        msm_dir = os.path.join(Microsim.DATA_DIR, "msm_data")
+        msm_dir = os.path.join(cls.DATA_DIR, "msm_data")
 
         # households
         house_dfs = []
@@ -97,7 +98,7 @@ class Microsim:
             house_dfs.append(pd.read_csv(f))
         if len(house_dfs) == 0:
             raise Exception(f"No household csv files found in {msm_dir}.",
-                            f"Have you downloaded and extracted the necessary data? (see {Microsim.DATA_DIR} README)")
+                            f"Have you downloaded and extracted the necessary data? (see {cls.DATA_DIR} README)")
         households = pd.concat(house_dfs)
 
         # individuals
@@ -106,7 +107,7 @@ class Microsim:
             indiv_dfs.append(pd.read_csv(f))
         if len(indiv_dfs) == 0:
             raise Exception(f"No individual csv files found in {msm_dir}.",
-                            f"Have you downloaded and extracted the necessary data? (see {Microsim.DATA_DIR} README)")
+                            f"Have you downloaded and extracted the necessary data? (see {cls.DATA_DIR} README)")
         individuals = pd.concat(indiv_dfs)
 
         # THE FOLLOWING SHOULD BE DONE AS PART OF A TEST SUITE
@@ -124,8 +125,8 @@ class Microsim:
 
         return (individuals, households)
 
-    @staticmethod
-    def attach_health_data(individuals):
+    @classmethod
+    def attach_health_data(cls, individuals: pd.DataFrame) -> pd.DataFrame:
         """
 
         :param individuals:
@@ -136,21 +137,108 @@ class Microsim:
         print("... finished.")
         return individuals
 
-    @staticmethod
-    def attach_time_use_data(individuals):
+    @classmethod
+    def attach_time_use_data(cls, individuals: pd.DataFrame) -> pd.DataFrame:
         print("Attaching time use data ... ", )
         pass
         print("... finished.")
         return individuals
 
-    @staticmethod
-    def attach_labour_force_data(individuals):
+    @classmethod
+    def attach_labour_force_data(cls, individuals: pd.DataFrame) -> pd.DataFrame:
         print("Attaching labour force ... ", )
         pass
         print("... finished.")
         return individuals
 
-    def step(self):
+    @classmethod
+    def read_retail_flows_data(cls) -> (pd.DataFrame, pd.DataFrame):
+        """
+        Read the flows between each MSOA and the most commonly visited shops
+
+        :return: A tuple of two dataframes. One containing all of the flows and another
+        containing information about the stores themselves.
+        """
+        dir = os.path.join(cls.DATA_DIR, "temp-retail")
+
+        # Read the stores
+        stores = pd.read_csv(os.path.join(dir, "devon smkt.csv"))
+        stores['ID'] = list(stores.index + 1)  # Mark counts from 1, not zero, so indices need to start from 1
+        stores = stores.set_index("ID", drop=True)
+
+        # Read the flows
+        rows = []  # Build up all the rows in the matrix gradually then add all at once
+        with open(os.path.join(dir, "DJR002.TXT")) as f:
+            count = 1  # Mark's file comes in batches of 3 lines, each giving different data
+
+            # See the README for info about these variables. This is only tempoarary so I can't be bothered
+            # to explain properly
+            oa = None
+            num_dests = None
+            dests = None
+            flows = None
+
+            for lineno, raw_line in enumerate(f):
+                # print(f"{lineno}: '{raw_line}'")
+                line_list = raw_line.strip().split()
+                if count == 1:  # OA and number of destinations
+                    oa = int(line_list[0])
+                    num_dests = int(line_list[1])
+                elif count == 2:  # Top N (=10) destinations in the OA
+                    # First number is the OA (don't need this), rest are the destinations
+                    assert int(line_list[0]) == oa
+                    dests = [int(x) for x in line_list[1:]]  # Make the destinations numbers
+                    assert len(dests) == num_dests
+                elif count == 3:  # Distance to each store (not currently used)
+                    pass
+                elif count == 4:  # Flows per 1,000 trips
+                    # First number is the OA (don't need this), rest are the destinations
+                    assert int(line_list[0]) == oa
+                    flows = [float(x) for x in line_list[1:]]  # Make the destinations numbers
+                    assert len(flows) == num_dests
+
+                    # Have read all information for this area. Store the info in the flows matrix
+
+                    # We should have one line in the matrix for each OA, and OA codes are incremental
+                    # assert len(flow_matrix) == oa - 1
+                    row = [0.0 for _ in range(len(stores))]  # Initially assume all flows are 0
+                    for i in range(num_dests):  # Now add the flows
+                        dest = dests[i]
+                        flow = flows[i]
+                        row[dest - 1] = flow  # (-1 because destinations are numbered from 1, not 0)
+                    assert len([x for x in row if x > 0]) == num_dests  # There should only be N >0 flows
+                    row = [oa, "NA"] + row  # Insert the OA number and code (don't know this yet now)
+
+                    rows.append(row)
+
+                    # Add the row to the matrix. As the OA numbers are incremental they should equal the number
+                    # of rows
+                    # flow_matrix.loc[oa-1] = row
+                    # assert len(flow_matrix) == oa
+                    count = 0
+
+                count += 1
+
+        # Have finished reading the file, now create the matrix. MSOAs as rows, retail locations as columns
+        columns = ["Area_ID", "Area_Code"]  # A number (ID) and full code for each MSOA
+        columns += [f"Loc_{i}" for i in stores.index]  # Columns for each store
+        flow_matrix = pd.DataFrame(data=rows, columns=columns)
+
+        return stores, flow_matrix
+
+    @classmethod
+    def add_individual_flows(cls, flow_type: str, individuals: pd.DataFrame, flows: pd.DataFrame) -> pd.DataFrame:
+        """
+        Take a flow matrix from MSOAs to (e.g. retail) locations and assign flows to individuals
+        :param flow_type: What type of flows are these. This will be appended to the column names. E.g. "Retail".
+        :param individuals: The DataFrame contining information about all individuals
+        :param flows: The flow matrix, created by (e.g.) read_retail_flows_data()
+        :return: The DataFrame of individuals with new locations and probabilities added
+        """
+        # TODO HERE
+        pass
+
+    def step(self) -> None:
         """Step (iterate) the model"""
         self.iteration += 1
         print(f"Iteration: {self.iteration}")
