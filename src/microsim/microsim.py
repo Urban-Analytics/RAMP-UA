890ff32da5c0dfc99af38208b43b310daf24918c
Nick Malleson <nick@geo-mac78.broadband>
2020-05-10 10:59:11 +0100

    added some more tests

--- src/microsim/microsim.py
+++ src/microsim/microsim.py
@@ -15,9 +15,8 @@ import random
 import time
 import typing
 
-import click # command-line interface
+import click  # command-line interface
 
-DATA_DIR = "../../data/"
 
 class Microsim:
     """
@@ -36,13 +35,25 @@ class Microsim:
     XXXX
     """
 
-    def __init__(self, random_seed:float = None):
+    DATA_DIR = "../../data/"
+
+    def __init__(self, random_seed: float = None, read_data: bool = True):
         """
         Microsim constructor.
         ----------
         :param random_seed: A optional random seed to use when creating the class instance. If None then
             the current time is used.
+        :param read_data: Optionally don't read in the data when instantiating this Microsim (useful
+            in debugging).
         """
+
+        # Administrative variables that need to be defined
+        self.iteration = 0
+        self.random = random.Random(time.time() if random_seed is None else random_seed)
+
+        # Now the main chunk of initialisation is to read the input data.
+        if not read_data:  # Optionally can not do this, usually for debugging
+            return
 
         # This is the main population of individuals and their households
         self.individuals, self.households = Microsim.read_msm_data()
@@ -50,58 +61,59 @@ class Microsim:
         # Attach a load of health attributes to each individual
         self.individuals = Microsim.attach_health_data(self.individuals)
 
+        # Attach the labour force data
+        self.individuals = Microsim.attach_labour_force_data(self.individuals)
+
         # Attach a load of transport attributes to each individual
-        self.individuals = Microsim.attach_transport_data(self.individuals)
+        self.individuals = Microsim.attach_time_use_data(self.individuals)
+
+        # Now we have the 'core population'. Keep a copy of this but continue to use the 'individuals' data frame
+        self.core_population = self.individuals.copy()
 
         # Read the locations of schools, workplaces, etc.
-        #self.schools = Microsim.read_school_data()
-        #self.shops = Microsim.read_shop_data()
-        #self.workplaces = Microsim.read_workplace_data()
+        # self.schools = Microsim.read_school_data()
+        self.shops = Microsim.read_shop_data()
+        # self.workplaces = Microsim.read_workplace_data()
 
         # Assign probabilities that each individual will go to each location (most of these will be 0!)
         # Do this by adding two new columns, both storing lists. One lists ids of the locations that
         # the individual may visit, the other has the probability of them visitting those places
 
+        print(".. End of __init__() .. ")
+        return
 
-
-        # Other variables that need to be defined
-        self.iteration = 0
-        self.random = random.Random(time.time() if random_seed is None else random_seed)
-    
     @staticmethod
     def read_msm_data():
         """Read the csv files that have the indivduls and households
 
         :return a tuple with two pandas dataframes representing individuls (0) and households (1)
         """
-        
-        msm_dir = os.path.join(DATA_DIR,"msm_data")
-    
+
+        msm_dir = os.path.join(Microsim.DATA_DIR, "msm_data")
+
         # households
         house_dfs = []
-        for f in glob.glob(msm_dir+'/ass_hh_*_OA11_2020.csv'):
+        for f in glob.glob(msm_dir + '/ass_hh_*_OA11_2020.csv'):
             house_dfs.append(pd.read_csv(f))
-        if len(house_dfs)==0:
+        if len(house_dfs) == 0:
             raise Exception(f"No household csv files found in {msm_dir}.",
-                            f"Have you downloaded and extracted the necessary data? (see {DATA_DIR} README)")
+                            f"Have you downloaded and extracted the necessary data? (see {Microsim.DATA_DIR} README)")
         households = pd.concat(house_dfs)
-        
+
         # individuals
-        indiv_dfs= []
-        for f in glob.glob(msm_dir+'/ass_*_MSOA11_2020.csv'):
+        indiv_dfs = []
+        for f in glob.glob(msm_dir + '/ass_*_MSOA11_2020.csv'):
             indiv_dfs.append(pd.read_csv(f))
         if len(indiv_dfs) == 0:
             raise Exception(f"No individual csv files found in {msm_dir}.",
-                            f"Have you downloaded and extracted the necessary data? (see {DATA_DIR} README)")
+                            f"Have you downloaded and extracted the necessary data? (see {Microsim.DATA_DIR} README)")
         individuals = pd.concat(indiv_dfs)
-        
+
         # THE FOLLOWING SHOULD BE DONE AS PART OF A TEST SUITE
-        #TODO: check that correct numbers of rows have been read.    
-        #TODO: check that each individual has a household    
-        #TODO: graph number of people per household just to sense check
-        
-        
-        
+        # TODO: check that correct numbers of rows have been read.
+        # TODO: check that each individual has a household
+        # TODO: graph number of people per household just to sense check
+
         print("Have read files:",
               f"\n\tHouseholds:  {len(house_dfs)} files with {len(households)}",
               f"households in {len(households.Area.unique())} areas",
@@ -119,40 +131,44 @@ class Microsim:
         :param individuals:
         :return:
         """
-        print("Attaching health data ... ",)
+        print("Attaching health data ... ", )
         pass
         print("... finished.")
         return individuals
 
     @staticmethod
-    def attach_transport_data(individuals):
-        print("Attaching health data ... ",)
+    def attach_time_use_data(individuals):
+        print("Attaching time use data ... ", )
         pass
         print("... finished.")
         return individuals
 
+    @staticmethod
+    def attach_labour_force_data(individuals):
+        print("Attaching labour force ... ", )
+        pass
+        print("... finished.")
+        return individuals
 
     def step(self):
         """Step (iterate) the model"""
-        self.iteration +=1 
+        self.iteration += 1
         print(f"Iteration: {self.iteration}")
         # XXXX HERE
-        
 
-    
-    
 
 # PROGRAM ENTRY POINT
 # Uses 'click' library so that it can be run from the command line
 @click.command()
 @click.option('--iterations', default=10, help='Number of model iterations')
 def run(iterations):
-    num_iter=iterations
+    num_iter = iterations
     m = Microsim()
     for i in range(num_iter):
         m.step()
     print("End of program")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     run()
     print("End of program")
--- src/microsim/test_microsim.py
+++ src/microsim/test_microsim.py
@@ -1,20 +1,43 @@
 import pytest
+import multiprocessing
 import microsim
 
+@pytest.fixture()
+def setup():
+    """Used to perform setup stuff that other tests can use"""
+    print("\n I'm the fixture - setUp", flush=True)
+    yield
+    print("\nI'm the fixture - tearDown", flush=True)
 
-def test_random():
+def _get_rand(microsim, N=100):
+    """Get a random number using the Microsimulation object's random number generator"""
+    for _ in range(N):
+        microsim.random.random()
+    return microsim.random.random()
+
+
+def test_random(setup):
     """
-    Checks that random classes are created properly
+    Checks that random classes are produce different (or the same!) numbers when they should do
     :return:
     """
-    m1 = microsim.Microsim()
-    m2 = microsim.Microsim(random_seed=2.0)
-    m3 = microsim.Microsim(random_seed=2.0)
+    m1 = microsim.Microsim(read_data=False)
+    m2 = microsim.Microsim(random_seed=2.0, read_data=False)
+    m3 = microsim.Microsim(random_seed=2.0, read_data=False)
 
     # Genrate a random number from each model. The second two numbers should be the same
-    r1, r2, r3 = [x.random.random() for x in [m1, m2, m3]]
+    r1, r2, r3 = [_get_rand(x) for x in [m1, m2, m3]]
 
     assert r1 != r2
     assert r2 == r3
+
+    # Check that this still happens even if they are executed in pools.
+    # Create a large number of microsims and check that all random numbers are unique
+    pool = multiprocessing.Pool()
+    m = [ microsim.Microsim(read_data=False) for _ in range(10000)]
+    r = pool.map( _get_rand, m)
+    assert len(r) == len(set(r))
+
+
 
 
