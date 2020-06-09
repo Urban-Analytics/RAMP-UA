900a9741f32b6cca4fcd554bfc3ac8a7e42d0af9
Nick Malleson <n.s.malleson@leeds.ac.uk>
2020-06-09 11:28:09 +0100

    renamed visualisations flag to "output"

--- microsim/microsim_initialisation.py
+++ microsim/microsim_initialisation.py
@@ -185,7 +185,7 @@ def run_script(repetitions, data_dir, init_dir, multiprocess, debug):
 
     # Initialise a model (MirosimInit init is a child of Microsim)
     m = MicrosimInit(msoa_danger=msoa_danger, cases=cases, results_dir=results_dir,
-                     study_msoas=list(devon_msoas.Code), data_dir=data_dir, visualisations=False, debug=debug)  # These are for the parent Microsim object
+                     study_msoas=list(devon_msoas.Code), data_dir=data_dir, output=False, debug=debug)  # These are for the parent Microsim object
 
     # Find a new directory for this initialisation (may have old ones)
     i = 0
