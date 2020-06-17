cdb6f3ee6eaf8cb5d268e6df164f8ee288fb91b2
jabrams23 <jabrams23@gmail.com>
2020-06-17 11:20:02 +0100

    create new directory

--- R/py_int/covid_run.R
+++ R/py_int/covid_run.R
@@ -144,8 +144,8 @@ run_status <- function(pop, timestep=1) {
     stat <- cbind(stat,tmp3)
   }
   #ncase <- as.data.frame(ncase)
-  write.csv(stat, paste("disease_status.csv",Sys.time(),".csv",sep=""))
-
+  write.csv(stat, paste(tmp.dir,"/disease_status.csv",sep=""))
+  
   return(df_out)
 }
 
