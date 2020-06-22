241b8f06224efa6be7572d3e59e6540607b5d8b9
nnelissen <natalie.nelissen@gmail.com>
2020-06-22 17:52:01 +0100

    danger normalisation, std

--- microsim/dashboard.py
+++ microsim/dashboard.py
@@ -90,20 +90,50 @@ for key, value in locations_dict.items():
     dangers_dict[key] = pd.DataFrame(data=dangers_3d.mean(axis=2), index=dangers_rownames, columns=dangers_colnames)
     dangers_dict_std[key] = pd.DataFrame(data=dangers_3d.std(axis=2), index=dangers_rownames, columns=dangers_colnames)
     
+    
+# how many days have we got
+nr_days = dangers_dict["Retail"].shape[1]
+days = [i for i in range(0,nr_days)]
+
+    
 # Add additional info about schools and retail including spatial coordinates
 # merge
 primaryschools = pd.merge(schools, dangers_dict["PrimarySchool"], left_index=True, right_index=True)
 secondaryschools = pd.merge(schools, dangers_dict["SecondarySchool"], left_index=True, right_index=True)
 retail = pd.merge(retail, dangers_dict["Retail"], left_index=True, right_index=True)
-    
 
-    
+
+# read in postcode to MSOA conversion doc
+data_file = os.path.join(data_dir, "PCD_OA_LSOA_MSOA_LAD_AUG19_UK_LU.csv")
+postcode_lu = pd.read_csv(data_file, encoding = "ISO-8859-1", usecols = ["pcds", "msoa11cd"])
+# creat LUT
+indices = postcode_lu.pcds # your values from 15 to 75
+results =  postcode_lu.msoa11cd # this constructs your ranges
+lookup = dict(zip(indices, results)) # zip together the lists and make a dict from it
+# use LUT and add column to retail variable
+msoa_code = [lookup.get(retail.postcode[i]) for i in range(1, len(retail.postcode)+1, 1)]
+retail.insert(2, 'MSOA_code', msoa_code)
+
+
+# normalised danger scores per msoa for schools and retail (for choropleth)
+dangers_msoa_dict = {}
+for key in ['Retail','PrimarySchool','SecondarySchool']:
+    dangers = dangers_dict[key]
+    if key == 'Retail':
+        msoa_code = retail.MSOA_code
+    else:
+        msoa_code = schools.MSOA_code
+    dangers['MSOA'] = msoa_code
+    # count nr for this condition per area
+    msoa_sum = dangers.groupby(['MSOA']).agg('sum')  
+    msoa_count = dangers.groupby(['MSOA']).agg('count')  
+    msoa_avg =  msoa_sum.div(msoa_count, axis='index')
+    dangers_msoa_dict[key] = msoa_avg
+
+   
     
 # Get nr people per condition, day and msoa
 # -----------------------------------------
-# how many days have we got
-nr_days = dangers_dict["Retail"].shape[1]
-days = [i for i in range(0,nr_days)]
 
 # initialise variables
 conditions_dict = {
@@ -117,11 +147,11 @@ conditions_dict = {
 msoacounts_dict = {}  # empty dictionary to store results: nr per msoa and day
 totalcounts_dict = {}  # empty dictionary to store results: nr per day
 cumcounts_dict = {}  # empty dictionary to store results: total nr across time period
+
 # time range for cumulative counts
 start_day = 0
 end_day = nr_days-1 # last 'named' day - nrs starts from 0
-start_col = individuals.shape[1] - nr_days + start_day
-end_col = individuals.shape[1] - nr_days + end_day + 1
+
 
 for r in range(nr_runs):
     # read in pickle file individuals (disease status)
@@ -134,6 +164,9 @@ for r in range(nr_runs):
         individuals = individuals_tmp
         msoas = sorted(individuals.area.unique())
         area_individuals = individuals['area']
+        start_col = individuals.shape[1] - nr_days + start_day
+        end_col = individuals.shape[1] - nr_days + end_day + 1
+        uniqcounts = np.zeros((individuals.shape[0],len(conditions_dict),nr_runs))   # empty dictionary to store results: total nr, each person assigned to highest condition: 0<1<2<(3==4)
         
     for key, value in conditions_dict.items():
         if r == 0:
@@ -148,14 +181,12 @@ for r in range(nr_runs):
         # create new df of zeros and replace with 1 at indices
         cumcounts_run = pd.DataFrame(np.zeros((tmp.shape[0], 1)))
         cumcounts_run.loc[indices] = 1
+        uniqcounts[:,value,r] = cumcounts_run.values[:,0]
         # merge with MSOA df
         cumcounts_run = cumcounts_run.merge(area_individuals, left_index=True, right_index=True)
-        cumcounts_msoa_run = mergeddf[mergeddf == conditions_dict[key]].groupby(['area']).agg({cumcounts_run.columns[0]: ['count']})  
+        cumcounts_msoa_run = cumcounts_run.groupby(['area']).sum()
         cumcounts_msoa_run = cumcounts_msoa_run.values
 
-        
-        
-        
         # loop aroud days
         msoacounts_run = np.zeros((len(msoas),nr_days))
         for d in range(0, nr_days):
@@ -186,39 +217,60 @@ for d in range(0, nr_days):
 # aggregate counts    
 msoacounts_dict_std = {}  
 totalcounts_dict_std = {}
+cumcounts_dict_std = {}
+
 msoacounts_dict_3d = dict(msoacounts_dict) 
+cumcounts_dict_3d = dict(cumcounts_dict) 
 for key, value in conditions_dict.items():
     # get current values from dict
     msoacounts = msoacounts_dict[key]
     totalcounts = totalcounts_dict[key]
+    cumcounts = cumcounts_dict[key]
     # aggregate
     msoacounts_std = msoacounts.std(axis=2)
     msoacounts = msoacounts.mean(axis=2)
     totalcounts_std = totalcounts.std(axis=1)
     totalcounts = totalcounts.mean(axis=1)
+    cumcounts_std = cumcounts.std(axis=1)
+    cumcounts = cumcounts.mean(axis = 1)
     # write out to dict
     msoacounts_dict[key] = pd.DataFrame(data=msoacounts, index=msoas, columns=dict_days)
-    totalcounts_dict[key] = pd.Series(data=totalcounts, index=dict_days)
     msoacounts_dict_std[key] = pd.DataFrame(data=msoacounts_std, index=msoas, columns=dict_days)
+    totalcounts_dict[key] = pd.Series(data=totalcounts, index=dict_days)
     totalcounts_dict_std[key] = pd.Series(data=totalcounts_std, index=dict_days)
-
-
+    cumcounts_dict[key] = pd.Series(data=cumcounts, index=msoas)
+    cumcounts_dict_std[key] = pd.Series(data=cumcounts_std, index=msoas)
+ 
     
+# TO BE COMPLETED - calculate unique nr people per condition 
+# uniqcounts_dict_3d = {}
+# for r in range(nr_runs):
+#     tmp = uniqcounts[:,4,r]
+#     result = np.where(tmp == 1)
+#     indices = result[0]
+#     # set other values on row to zero
+
+
 
 
 msoas_nr = [i for i in range(0,len(msoas))]
         
 # threshold map to only use MSOAs currently in the study or selection
 map_df = map_df[map_df['Area'].isin(msoas)]
-        
+
+# to normalise counts, we need to know the total nr of people per MSOA
+nrpeople_msoa = individuals[['ID','area']].groupby(['area']).count()
+nrpeople_msoa.rename(columns={'ID':'nr_people'}, inplace=True)
+
+
     
- 
+
 
 
 
 
 # Determine where the visualization will be rendered
-output_file('dashboard_v1.html', title='RAMP-UA microsim output') # Render to static HTML
+output_file('dashboard.html', title='RAMP-UA microsim output') # Render to static HTML
 #output_notebook()  # To tender inline in a Jupyter Notebook
 
 # default tools for  plots
@@ -259,6 +311,9 @@ for key, value in dangers_dict.items():
 plotref_dict = {}
 
    
+
+
+
     
 # plot 1: heatmap
 
@@ -307,12 +362,27 @@ for key,value in conditions_dict.items():
 # build ColumnDataSource
 data_s2 = dict(totalcounts_dict)
 data_s2["days"] = days
+for key, value in totalcounts_dict.items():
+    #totalcounts_dict_std[f"{key}_std"] = totalcounts_dict_std.pop(f"{key}")
+    data_s2[f"{key}_std_upper"] = totalcounts_dict[key] + totalcounts_dict_std[key]
+     #x.add(y, fill_value=0)
+    data_s2[f"{key}_std_lower"] = totalcounts_dict[key] - totalcounts_dict_std[key]
+#data_s2.update(totalcounts_dict_std)
 source_2 = ColumnDataSource(data=data_s2)
 
 s2 = figure(background_fill_color="#fafafa",title="Time", x_axis_label='Time', y_axis_label='Nr of people')
 for key, value in totalcounts_dict.items():
     s2.line(x = 'days', y = key, source = source_2, legend_label=f"nr {key}", line_width=2, line_color=colour_dict[key])
-    s2.circle(x = 'days', y = key, source = source_2, fill_color=colour_dict[key], line_color=colour_dict[key], size=5,legend_label=f"nr {key}")
+    s2.square(x = 'days', y = key, source = source_2, fill_color=colour_dict[key], line_color=colour_dict[key], size=5,legend_label=f"nr {key}")
+    
+    # s2.rect('days', f"{key}_std_upper", 0.2, 0.01, line_color="black")
+    # s2.rect('days', f"{key}_std_lower", 0.2, 0.01, line_color="black")
+    # s2.segment('days', f"{key}_std_lower", 'days', f"{key}_std_upper", line_color="black")
+#     # find the outliers
+#     y3 = y[(y > y2+stdev) | (y < y2-stdev)]
+#     x3 = x[0:len(y3)]
+#     s5.circle(x = x3, y = y3, fill_color="black", size=5)
+    
 
 tooltips = tooltips_cond_basic.copy()
 tooltips.append(tuple(( 'Day',  '@days' )))
@@ -324,14 +394,34 @@ s2.legend.click_policy="hide"
 
 
 
+
+
+#     y = msoacounts_dict_3d[key][msoa,day,:]
+#     x = np.ones(10)
+#     x = [i * value for i in x]
+#     # s5.circle(x = x, y = y, fill_color=colour_dict[key], size=3,legend_label=f"nr {key}")
+#     y2 = y.mean()
+#     x2 = value
+#     s5.square(x = x2, y = y2, line_color="black", size=5)
+#     stdev = y.std()
+#     s5.rect(x, y2-stdev, 0.2, 0.01, line_color="black")
+#     s5.rect(x, y2+stdev, 0.2, 0.01, line_color="black")
+#     s5.segment(x, y2-stdev, x, y2+stdev, line_color="black")
+#     # find the outliers
+#     y3 = y[(y > y2+stdev) | (y < y2-stdev)]
+#     x3 = x[0:len(y3)]
+#     s5.circle(x = x3, y = y3, fill_color="black", size=5)
+
+
+
 # plot 3: Conditions across MSOAs
 
 # build ColumnDataSource
 data_s3 = {}
 data_s3["msoa_nr"] = msoas_nr
 data_s3["msoa_name"] = msoas
-for key, value in msoacounts_dict.items():
-    data_s3[key] = msoacounts_dict[key].sum(axis = 1)
+for key, value in cumcounts_dict.items():
+    data_s3[key] = cumcounts_dict[key]
 source_3 = ColumnDataSource(data=data_s3)
 
 s3 = figure(background_fill_color="#fafafa",title="MSOA", x_axis_label='Nr people', y_axis_label='MSOA',toolbar_location='above')
@@ -358,7 +448,8 @@ data_s4 = data_s3.copy()
 merged_data = pd.DataFrame()
 merged_data['Area'] = msoas
 for key, value in msoacounts_dict.items():
-    merged_data[key] = msoacounts_dict[key].sum(axis = 1).to_list()
+    #merged_data[key] = msoacounts_dict[key].sum(axis = 1).to_list()
+    merged_data[key] = cumcounts_dict[key].to_list()
 merged_data = pd.merge(map_df,merged_data,on='Area')
 # Turn data into GeoJSON source
 geosource = GeoJSONDataSource(geojson = merged_data.to_json())
@@ -506,7 +597,55 @@ s5.legend.click_policy="hide"
 
 
 
+# plot 7: choropleth dangers
 
+# # merge counts with spatial data
+# data_s4 = data_s3.copy()
+# merged_data = pd.DataFrame()
+# merged_data['Area'] = msoas
+# for key, value in dangers_msoa_dict.items():
+#     #merged_data[key] = msoacounts_dict[key].sum(axis = 1).to_list()
+#     merged_data[key] = cumcounts_dict[key].to_list()
+# merged_data = pd.merge(map_df,merged_data,on='Area')
+# # Turn data into GeoJSON source
+# geosource = GeoJSONDataSource(geojson = merged_data.to_json())
+
+# def plot_choropleth_condition(condition2plot):
+#     """ Create choropleth: colour = nr people with condition = condition2plot. condition2plot is key to conditions_dict. """
+
+#     # Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors.
+#     mapper_4 = LinearColorMapper(palette = palette, low = 0, high = merged_data[condition2plot].max())
+#     # Create color bar.
+#     color_bar_4 = ColorBar(color_mapper = mapper_4, 
+#                          label_standoff = 8,
+#                          #"width = 500, height = 20,
+#                          border_line_color = None,
+#                          location = (0,0), 
+#                          orientation = 'horizontal')#,
+#                          #major_label_overrides = tick_labels)
+    
+#     # Create figure object.
+#     s4 = figure(title = f"{condition2plot} total")
+#     s4.xgrid.grid_line_color = None
+#     s4.ygrid.grid_line_color = None
+#     # Add patch renderer to figure.
+#     msoasrender = s4.patches('xs','ys', source = geosource,
+#                        fill_color = {'field' :condition2plot,
+#                                      'transform' : mapper_4},     
+#                        #fill_color = None,
+#                        line_color = 'gray', 
+#                        line_width = 0.25, 
+#                        fill_alpha = 1)
+#     # Create hover tool
+#     s4.add_tools(HoverTool(renderers = [msoasrender],
+#                           tooltips = [('MSOA','@Area'),
+#                                     (f'Nr {condition2plot} people',f'@{condition2plot}')]))
+#     s4.add_layout(color_bar_4, 'below')
+
+#     plotref_dict[f"ch{condition2plot}"] = s4
+
+# for key,value in conditions_dict.items():
+#     plot_choropleth_condition(key)
 
 
 
@@ -523,6 +662,8 @@ tab6 = Panel(child=row(plotref_dict["hmdead"],plotref_dict["chdead"]), title='De
 # tab7 = Panel(child=row(plotref_dict["hmRetail"],plotref_dict["hmPrimarySchool"]), title='test')
 tab7 = Panel(child=row(s5), title='Venue dangers')
 
+# tab8 = Panel(child=row(s5), title='Venue dangers')
+
 # Put the Panels in a Tabs object
 tabs = Tabs(tabs=[tab1, tab2, tab3, tab4, tab5, tab6, tab7])
 show(tabs)
@@ -536,3 +677,27 @@ show(tabs)
 # ])
 
 # show(l)
+
+# day = 20
+# msoa = 0
+
+# s5 = figure(background_fill_color="#fafafa",title="Variability", x_axis_label='Condition', y_axis_label='Nr people')
+
+# for key, value in conditions_dict.items():
+#     y = msoacounts_dict_3d[key][msoa,day,:]
+#     x = np.ones(10)
+#     x = [i * value for i in x]
+#     # s5.circle(x = x, y = y, fill_color=colour_dict[key], size=3,legend_label=f"nr {key}")
+#     y2 = y.mean()
+#     x2 = value
+#     s5.square(x = x2, y = y2, line_color="black", size=5)
+#     stdev = y.std()
+#     s5.rect(x, y2-stdev, 0.2, 0.01, line_color="black")
+#     s5.rect(x, y2+stdev, 0.2, 0.01, line_color="black")
+#     s5.segment(x, y2-stdev, x, y2+stdev, line_color="black")
+#     # find the outliers
+#     y3 = y[(y > y2+stdev) | (y < y2-stdev)]
+#     x3 = x[0:len(y3)]
+#     s5.circle(x = x3, y = y3, fill_color="black", size=5)
+
+# show(s5)
