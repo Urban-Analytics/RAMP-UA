15ab8600e5a6b0c9cbee951d2e2b723635eb1e3b
Fiona Spooner <spoonerfiona@gmail.com>
2020-07-23 09:26:09 +0100

    code to get google mobility lockdown multiplier for all LAD populations - except Rutland

--- R/py_int/google_mobility_all_LAD.R
+++ R/py_int/google_mobility_all_LAD.R
@@ -1,3 +1,5 @@
+devtools::install_github("Urban-Analytics/rampuaR", force = TRUE)
+
 library(readr)
 library(dplyr)
 library(tidyr)
@@ -5,29 +7,37 @@ library(rampuaR)
 library(stringr)
 
 ### download google mobility data and local authority codes 
-gm_file_download()
+gm_file_download(force_gm = TRUE)
 lad_file_download()
+county_file_download()
+county_codes <- read_csv("lad_county_codes.csv")
 
 lad_codes <- read_csv("lad_codes.csv") %>% 
-  distinct()
+  distinct() %>% 
+  left_join(., county_codes, by = "OA11CD")
 
 gm <- read_csv("Google_Global_Mobility_Report.csv") %>% 
   filter(country_region == "United Kingdom" & !is.na(sub_region_1))
 
 all_lad <- list.files("devon_data/FullMatchedData_LAD", full.names = TRUE)
 
-for(i in 80:length(all_lad)){
+seq_lad <- (1:length(all_lad))[-79] #skipping rutland for now
+
+for (i in seq_lad){
   print(i)
-  lad <- read_csv(all_lad[i])
+  lad <- read_csv(all_lad[i], col_types = cols())
   
-  lad_df <- msoa_lad_code_matcher(pop = lad)
+  lad_df <- msoa_lad_code_matcher(pop = lad, lad_codes = lad_codes)
   print(lad_df$lad_name[1])
   
-  gm_out <- gm_filter(lad_df)
+  gm_filt <- gm_filter(gm,lad_name = unique(pop$lad_name), county_name = unique(pop$county))
+  print(gm_out$sub_region_1[1])
   
-  if(length(gm_out) > 1){
-    smth_res <- residential_smoother(gm_out)
-    
+  print(paste(lad_df$lad_name[1], gm_out$sub_region_1[1]))
+  
+  if(length(gm_filt) > 1){
+    gm_formatted <- format_gm(gm_filt)
+    smth_res <- residential_smoother(gm_formatted)
     lockdown_out <- lockdown_multiplier(smth_res = smth_res, pop = lad_df)
     
     write.csv(lockdown_out, paste0("devon_data/GoogleMobilityLAD/", gsub(".txt", ".csv",basename(all_lad[i]))))
@@ -36,8 +46,11 @@ for(i in 80:length(all_lad)){
 }
 
 
+#Stroud
+#Rutland 80
 
-lad <- read_csv("~/Documents/ECEHH/COVID/RAMP-UA//devon_data/FullMatchedData_LAD/lad_tus_hse_100.txt")
+
+
 
 
 
