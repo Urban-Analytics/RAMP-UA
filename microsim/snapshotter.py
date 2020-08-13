0b4042fc8fb362c90b44586565596f1242db07ed
rory-improbable <rory@improbable.io>
2020-08-13 16:02:52 +0100

    cache activity names

--- microsim/snapshotter.py
+++ microsim/snapshotter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+import pickle
 
 
 class Snapshotter:
@@ -12,15 +13,21 @@ class Snapshotter:
     def __init__(self, individuals, activity_locations, snapshot_dir, use_cache=True):
         self.snapshot_dir = snapshot_dir
 
-        self.individuals = self.cache_or_load(individuals, "individuals_cache.pkl") if use_cache else individuals
+        self.individuals = self.cache_or_load(individuals,
+                                                        "individuals_cache.pkl") if use_cache else individuals
+
+        self.activity_names = self.cache_or_load(activity_locations.keys(), "activity_names.pkl") if use_cache \
+            else activity_locations.keys()
 
         self.locations = dict()
-        for activity_name, activity_location in activity_locations.items():
+        for activity_name in self.activity_names:
             if use_cache:
                 cache_filename = "activity_locations_" + activity_name + "_cache.pkl"
-                self.locations[activity_name] = self.cache_or_load(activity_location._locations, cache_filename)
+                self.locations[activity_name] = self.cache_or_load(
+                    activity_locations[activity_name]._locations,
+                    cache_filename)
             else:
-                self.locations[activity_name] = activity_location._locations
+                self.locations[activity_name] = activity_locations[activity_name]._locations
 
     def store_snapshots(self):
         self.store_people_snapshots()
@@ -41,10 +48,16 @@ class Snapshotter:
         cache_filepath = os.path.join(self.snapshot_dir, cache_filename)
         if data is None:
             print(f"Reading cached data from {cache_filepath}")
-            return pd.read_pickle(cache_filepath)
+            if isinstance(data, pd.DataFrame):
+                return pd.read_pickle(cache_filepath)
+            else:
+                return pickle.load(open(cache_filepath, "rb"))
         else:
             print(f"Writing cache data to {cache_filepath}")
-            data.to_pickle(cache_filepath)
+            if isinstance(data, pd.DataFrame):
+                data.to_pickle(cache_filepath)
+            else:
+                pickle.dump(data, open(cache_filepath, "wb"))
             return data
 
 
