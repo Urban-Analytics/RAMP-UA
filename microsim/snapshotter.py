6a6f3b9652346d40742987a113b1eb0a284d6bcb
rory-improbable <rory@improbable.io>
2020-08-14 13:02:42 +0100

    code to get place ids and flows

--- microsim/snapshotter.py
+++ microsim/snapshotter.py
@@ -37,20 +37,84 @@ class Snapshotter:
                 if cache_inputs:
                     self.write_to_cache(cache_filename, self.locations[activity_name], is_dataframe=True)
 
+        self.num_people = self.individuals['ID'].count()
+        self.global_place_id_lookup = self.create_global_place_ids()
+
     def store_snapshots(self):
-        self.store_people_snapshots()
-        self.store_place_snapshots()
+        people_place_ids = self.get_people_place_data()
+        ages = self.get_people_ages()
 
-    def store_people_snapshots(self):
-        num_people = self.individuals['ID'].count()
-        ages = self.individuals['age']
-        age_array = ages.to_numpy(dtype=np.uint16)
-        filepath = os.path.join(self.snapshot_dir, 'people_ages.npz')
-        print(f"Saving ages for {num_people} people to {filepath}")
-        np.savez(filepath, age_array=age_array)
+        filepath = os.path.join(self.snapshot_dir, 'people_snapshot.npz')
+        print(f"Saving data for {self.num_people} people to {filepath}")
+        np.savez(filepath, ages=ages, people_place_ids=people_place_ids)
 
-    def store_place_snapshots(self):
-        pass
+    def create_global_place_ids(self):
+        max_id = 0
+        global_place_id_lookup = dict()
+
+        for activity_name in self.activity_names:
+            activity_ids = self.locations[activity_name]['ID'].to_numpy(dtype=np.uint32)
+            num_activity_ids = activity_ids.shape[0]
+            starting_id = activity_ids[0]
+
+            # check that activity IDs increase by one
+            assert activity_ids[-1] == num_activity_ids - 1 + starting_id
+
+            # subtract starting ID in case the first local activity ID is not zero
+            global_ids = activity_ids + max_id - starting_id
+            global_place_id_lookup[activity_name] = {
+                "ids": global_ids,
+                "id_offset": starting_id
+            }
+            max_id += num_activity_ids
+
+        return global_place_id_lookup
+
+    def get_global_place_id(self, activity_name, local_place_id):
+        ids_for_activity = self.global_place_id_lookup[activity_name]
+        global_id_location = local_place_id - ids_for_activity["id_offset"]
+        return ids_for_activity["ids"][global_id_location]
+
+    def get_people_ages(self):
+        return self.individuals['age'].to_numpy(dtype=np.uint16)
+
+    def get_people_place_data(self):
+        max_places_per_person = 16
+        people_place_ids = np.full((self.num_people, max_places_per_person), -1, dtype=np.uint32)
+        people_flows = np.full((self.num_people, max_places_per_person), -1, dtype=np.uint32)
+
+        for people_id in range(self.num_people):
+            person_global_place_ids = list()
+            person_place_flows = list()
+
+            for activity_name in self.activity_names:
+                activity_venue_col = activity_name + "_Venues"
+                local_place_ids = np.array(self.individuals.at[people_id, activity_venue_col], dytype=np.uint32)
+
+                activity_flow_col = activity_name + "_Flows"
+                activity_durations_col = activity_name + "_Duration"
+                activity_duration = np.uint32(self.individuals.at[people_id, activity_durations_col], dytype=np.uint32)
+                activity_flows = np.array(self.individuals.at[people_id, activity_flow_col], dytype=np.uint32)
+                activity_flows = activity_flows * activity_duration
+
+                # check dimensions match
+                assert local_place_ids.shape[0] == activity_flows.shape[0]
+
+                person_place_flows += activity_flows
+
+                for (i, local_place_id) in enumerate(local_place_ids):
+                    person_global_place_ids.append(self.get_global_place_id(activity_name, local_place_id))
+
+            person_place_data = np.array(list(zip(person_global_place_ids, person_place_flows)))
+            # sort by flow value descending so we can take the n places with highest flows
+            person_place_data = person_place_data[person_place_data[:, 1].argsort()[::-1]]
+            person_place_data = person_place_data[:max_places_per_person]
+
+            num_places = person_place_data.shape[0]
+            people_place_ids[people_id][0:num_places] = person_place_data[:, 0]
+            people_flows[people_id][0:num_places] = person_place_data[:, 1]
+
+        return people_place_ids, people_flows
 
     def load_from_cache(self, cache_filename, is_dataframe=False):
         cache_filepath = os.path.join(self.snapshot_dir, cache_filename)
