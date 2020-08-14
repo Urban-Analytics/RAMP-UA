bdbe414adfaf9252c550f4d71a1cc770af0396f0
jabrams23 <jabrams23@gmail.com>
2020-08-14 16:26:13 +0100

    added tests

--- tests/testthat/testR.R
+++ tests/testthat/testR.R
@@ -1,5 +1,5 @@
 test_that("load_rpackages works",{
-
+  test_check("rampuaR")
 })
 
 
@@ -14,7 +14,10 @@ test_that("initialize_r works", {
 
 
 test_that("run_status works", {
+  df <- data.frame(presymp_days = sample(1:10, size = 10, replace = TRUE),
+                   symp_days = sample(5:15, size = 10, replace = TRUE),
+                   disease_status = c(rep(0,5), 1, 2, 3, 4, 0),
+                   current_risk  = runif(10, 0, 1))
 
+  expect_type(run_status(pop = df)[["betaxs"]], "list")
 })
-
-test_check("rampuaR")
