b5b3182f3450ac237a442e3435c8d015f60c2181
rory-improbable <rory@improbable.io>
2020-08-17 18:34:13 +0100

    add extra person to test data

--- tests/test_snapshotter.py
+++ tests/test_snapshotter.py
@@ -9,21 +9,21 @@ class TestActivityLocation:
         self._locations = locations
 
 
-people_data = {'ID': [0, 1],
-               'age': [43, 22],
-               'Home_Venues': [[0], [1]],
-               'Home_Flows': [[1.0], [1.0]],
-               'Home_Duration': [0.8, 0.7],
-               'Retail_Venues': [[0, 2, 4], [1, 2, 3]],
-               'Retail_Flows': [[0.2, 0.5, 0.3], [0.1, 0.6, 0.3]],
-               'Retail_Duration': [0.2, 0.3]
+people_data = {'ID': [0, 1, 3],
+               'age': [43, 22, 33],
+               'Home_Venues': [[0], [1], [2]],
+               'Home_Flows': [[1.0], [1.0], [1.0]],
+               'Home_Duration': [0.8, 0.7, 0.6],
+               'Retail_Venues': [[0, 2, 4], [1, 2, 3], [0, 3, 4]],
+               'Retail_Flows': [[0.2, 0.5, 0.3], [0.1, 0.6, 0.3], [0.5, 0.1, 0.4]],
+               'Retail_Duration': [0.2, 0.3, 0.4]
                }
 
 individuals_df = pd.DataFrame(people_data,
                               columns=['ID', 'age', 'Home_Venues', 'Home_Duration', 'Home_Flows', 'Retail_Venues',
                                        'Retail_Duration', 'Retail_Flows'])
 
-home_data = {'ID': [0, 1]}
+home_data = {'ID': [0, 1, 2]}
 home_df = pd.DataFrame(home_data, columns=['ID'])
 
 retail_data = {'ID': [0, 1, 2, 3, 4]}
@@ -47,10 +47,14 @@ def test_global_id_lookup():
 
 
 def test_processes_people_flows():
-    expected_people_place_ids = np.array([[0, 4, 6, 2, 0, 0, 0, 0, 0, 0],
-                                          [1, 4, 5, 3, 0, 0, 0, 0, 0, 0]])
-    expected_people_flows = np.array([[0.8, 0.25, 0.15, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                                      [0.7, 0.18, 0.09, 0.03, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
+    expected_people_place_ids = np.array([[0, 5, 7, 3, 0, 0, 0, 0, 0, 0],
+                                          [1, 5, 6, 4, 0, 0, 0, 0, 0, 0],
+                                          [2, 3, 7, 6, 0, 0, 0, 0, 0, 0]
+                                          ])
+    expected_people_flows = np.array([[0.8, 0.1, 0.06, 0.04, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                                      [0.7, 0.18, 0.09, 0.03, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                                      [0.6, 0.2, 0.16, 0.04, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+                                      ])
 
     people_place_ids, people_flows = snapshotter.get_people_place_data()
 
