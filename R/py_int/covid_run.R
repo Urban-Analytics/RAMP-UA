3d4cd8ebccf1ac6aa11e98b75c3e4728044c99c5
Fiona Spooner <spoonerfiona@gmail.com>
2020-09-11 13:37:05 +0100

    using master rampuar pkg now

--- R/py_int/covid_run.R
+++ R/py_int/covid_run.R
@@ -3,14 +3,13 @@ load_rpackages <- function() {
   list.of.packages <- c("rampuaR")
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 
-  if(length(new.packages)) devtools::install_github("Urban-Analytics/rampuaR", dependencies = F, ref = "overweight_scenario")
+  if(length(new.packages)) devtools::install_github("Urban-Analytics/rampuaR", dependencies = F)
 
   library(rvcheck)
 
   rampr_version <- check_github("Urban-Analytics/rampuaR")
-  if(!rampr_version$up_to_date) devtools::install_github("Urban-Analytics/rampuaR", dependencies = F, ref = "overweight_scenario")
+  if(!rampr_version$up_to_date) devtools::install_github("Urban-Analytics/rampuaR", dependencies = F)
 
-  
   library(tidyr)
   library(readr)
   library(mixdist)
