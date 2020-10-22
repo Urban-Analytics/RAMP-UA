b9b371ed34da7d98c528e6d595f5fe28f1a8568c
Alex Coleman <alexjcoleman@gmail.com>
2020-10-22 14:29:58 +0100

    adjusted path names in sh file

--- examples/colab/ramp-colab.sh
+++ examples/colab/ramp-colab.sh
@@ -11,7 +11,9 @@ install_ramp () {
     chmod +x Miniconda3-4.5.4-Linux-x86_64.sh
     bash ./Miniconda3-4.5.4-Linux-x86_64.sh -b -f -p /usr/local
 
-    conda env create -p /usr/local -f ramp-ua/examples/colab/ramp-colab.yml
+    conda env create -p /usr/local -f RAMP-UA/examples/colab/ramp-colab.yml
+
+    python RAMP-UA/setup.py install
 
     echo "RAMP-UA conda environment created!"
 
