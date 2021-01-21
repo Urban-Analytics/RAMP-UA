d4eaa5205a60e918547943eafb0f27a473232058
Alex Coleman <alexjcoleman@gmail.com>
2021-01-21 11:57:07 +0000

    corrected tests for download_data

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -6,14 +6,20 @@ from microsim.utilities import download_data
 class TestDownloadData(unittest.TestCase):
 
     def test_download_success(self):
+        """
+        A test to fetch a dummy data tar file using the download data function in utils
+        """
         open_mock = mock_open()
 
-        with patch("__main__.open", open_mock):
-            download_data()
+        with patch("builtins.open", open_mock):
+            download_data(url="https://example0blob0store.blob.core.windows.net/test1/dummy_data.tar.gz")
         
         open_mock.assert_called_with("devon_data.tar.gz","wb")
 
     def test_download_fail(self):
+        """
+        A test the confirms an exception is raised if an invalid url is provided to download data
+        """
 
         with self.assertRaises(Exception):
             download_data(url="not_a_url")
