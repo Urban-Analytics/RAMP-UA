00c042e3bdb7602ddf26e6b7d6f2fcea65bdd942
Alex Coleman <alexjcoleman@gmail.com>
2021-01-28 10:24:53 +0000

    removed all devon data updated gitignore

--- devon_data/QUANT_RAMP/add_msoa_to_venue.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:29527ca2190d88ee4ebf3820397a0c84a04298e8fe65dcddc935eb2ed2879232
-size 3078
--- devon_data/README.md
+++
@@ -1,3 +0,0 @@
-# Data to run the microsim on Devon.
-
-The data are stored using [Git Large File Storage (LFS)](https://git-lfs.github.com/)
