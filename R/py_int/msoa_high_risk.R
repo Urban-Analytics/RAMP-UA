a78f0646a798b45b824853156511c61368fecda1
Fiona Spooner <spoonerfiona@gmail.com>
2021-02-10 15:19:29 +0000

    complete code for getting high risk MSOAs, still devon focussed but easy to change

--- R/py_int/msoa_high_risk.R
+++ R/py_int/msoa_high_risk.R
@@ -19,39 +19,33 @@ if(!file.exists("devon_data/income_ahc.csv")){
 }
 
 
-income <- clean_names(read_csv("devon_data/income_ahc.csv", skip = 3, skip_empty_rows = TRUE, col_names = TRUE)) ## having a lot of trouble with the column names, perhaps the £ signs
-colnames(income) <- make_clean_names(income[1,]) 
-income <- income[-1,]
-
-income <- income %>% 
-  filter(complete.cases(.)) %>% 
-  mutate(net_annual_income_after_housing_costs = as.numeric(gsub(",", "", net_annual_income_after_housing_costs))) %>% # removing comma and converting char to numeric
-  select(msoa_code, net_annual_income_after_housing_costs)
-
 if(!file.exists("devon_data/lsoa_to_msoa.csv")){
   download.file("https://opendata.arcgis.com/datasets/fe6c55f0924b4734adf1cf7104a0173e_0.csv", "devon_data/lsoa_to_msoa.csv")
 }
 
 lsoa_to_msoa <- clean_names(read_csv("devon_data/lsoa_to_msoa.csv")) 
 
-qimd <- clean_names(readxl::read_xlsx("~/Documents/ECEHH/COVID/xmsmcv/Raw_Data/Economic_Data/File_1_-_IMD2019_Index_of_Multiple_Deprivation.xlsx", sheet = "IMD2019")) %>% 
+if(!file.exists("devon_data/msoa_imd.xlsx")){
+  download.file("https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/833970/File_1_-_IMD2019_Index_of_Multiple_Deprivation.xlsx","devon_data/msoa_imd.xlsx")
+}
+
+
+qimd <- clean_names(readxl::read_xlsx("devon_data/msoa_imd.xlsx", sheet = "IMD2019")) %>% 
   left_join(., lsoa_to_msoa, by = c("lsoa_code_2011" = "lsoa11cd")) %>% 
   group_by(msoa11cd) %>% 
   summarise(mean_imd_rank = mean(index_of_multiple_deprivation_imd_rank))
 
 
 population_in <- population %>% 
-  left_join(., income, by = c("area" = "msoa_code")) %>% 
   left_join(., pop_dens, by = c("area" = "msoa_area_codes")) %>% 
   left_join(., connectivity, by = c("area" = "msoa11cd")) %>% 
   left_join(., qimd, by = c("area" = "msoa11cd")) %>% 
-  select(area, income = net_income_after_housing_costs, pop_dens = pop_dens_km2, connectivity = connectedness, qimd = mean_imd_rank) %>% 
+  select(area, pop_dens = pop_dens_km2, connectivity = connectedness, qimd = mean_imd_rank) %>% 
   distinct()
 
 
 groups <- 3
 
-population_in$income_ntile <- ntile(population_in$income,groups)
 population_in$pop_dens_ntile <- ntile(population_in$pop_dens,groups)
 population_in$connectivity_ntile <- ntile(population_in$connectivity, groups)
 population_in$qimd_ntile <- ntile(population_in$qimd, groups)
@@ -60,17 +54,10 @@ population_in$qimd_ntile <- ntile(population_in$qimd, groups)
 haz_msoas <- population_in %>%
   mutate(
     risk = case_when(
-      income_ntile == groups & pop_dens_ntile == groups & connectivity_ntile == groups & qimd_ntile == groups ~ "High",
-      income_ntile == 1 & pop_dens_ntile == 1 & connectivity_ntile == 1 & qimd_ntile == 1 ~ "Low"
+      pop_dens_ntile == groups & connectivity_ntile == groups & qimd_ntile == groups ~ "High",
+      pop_dens_ntile == 1 & connectivity_ntile == 1 & qimd_ntile == 1 ~ "Low"
     ), risk = ifelse(is.na(risk), "Medium", risk)) %>% 
   dplyr::select(area, risk)
 
 
-
 write.csv(haz_msoas, "msoa_danger_fn.csv", row.names = FALSE)
-
-write.csv(new_cases, "devon_cases_fn.csv", row.names = FALSE)
-
-
-unique(lsoa_to_msoa$msoa11nm[lsoa_to_msoa$msoa11cd %in% haz_msoas$area[haz_msoas$risk == "High"]])
-
