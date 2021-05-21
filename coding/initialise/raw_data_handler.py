c212e1dd8bacf2d39fb4219689c4c5cf922cdea8
ciupava <ciupava@gmail.com>
2021-05-21 13:23:30 +0100

    Still dealing with solving the unpacking og TU gz files

--- coding/initialise/raw_data_handler.py
+++ coding/initialise/raw_data_handler.py
@@ -63,17 +63,20 @@ class RawDataHandler:
         tus_hse = pd.DataFrame()
         # initially only try with the WYtest TUH file (fake ad-hoc file)
         for x in tus_hse_ref:
-            if not os.path.isfile(Constants.Paths.TU.FULL_PATH_FILE + x + ".gz"):
+            if not os.path.isfile(Constants.Paths.TU.FULL_PATH_FILE + x + ".csv"):
                 print("Downloading the TU files")
-                file_name = "countydata" + Constants.Paths.TU.FILE + x + ".gz"
+                file_name = os.path.join("countydata", Constants.Paths.TU.FILE + x + ".gz")
                 temp_path = RawDataHandler.download_data("countydata",
                                            Constants.Paths.TU.FILE + x + ".gz")
                 print(f"TU path {file_name}")
-                temp = pd.read_csv(temp_path)
+                print("Unpacking TU files")
+                # unpack_path = Constants.Paths.TU.FILE + x + ".gz"
+                unpacked_file = RawDataHandler.unpack_data(temp_path)
+                temp = pd.read_csv(unpacked_file)
             else:
                 print("Reading the TU files")
-                file_name = Constants.Paths.TU.FULL_PATH_FILE + x + ".gz"
-                temp = pd.read_csv(Constants.Paths.TU.FULL_PATH_FILE + x + ".gz")
+                file_name = Constants.Paths.TU.FULL_PATH_FILE + x + ".csv"
+                temp = pd.read_csv(Constants.Paths.TU.FULL_PATH_FILE + x + ".csv")
                 print(f"File is {file_name}")
             # temp = temp[temp.MSOA11CD.isin(msoasList)]
             # temp = temp[temp.area.isin(msoasList)]
@@ -219,7 +222,7 @@ class RawDataHandler:
             file (str): name of the file, must include the extension.
         """
         url = os.path.join(Constants.Paths.AZURE_URL + folder, file)  # TO_DO: does this work written like this?
-        target_path = os.path.join(Constants.Paths.DATA.FULL_PATH_FOLDER,
+        target_path = os.path.join(Constants.Paths.COUNTY_DATA.FULL_PATH_FOLDER,
                                    file)
         response = requests.get(url, stream=True)
         if not response.ok: # Ie checking that the HTTP status code is 'OK'
