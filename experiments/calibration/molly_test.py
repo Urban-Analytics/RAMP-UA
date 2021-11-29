7aa058b60254c6b71e34f6a96e6cc4867660e271
Molly <m.asher@hotmail.com>
2021-11-29 14:52:25 +0000

    implementing multiple model windows: feeding the posterior from the first run back in as a prior for the second run. With each window, run the model from the start of the time period

--- experiments/calibration/molly_test.py
+++ experiments/calibration/molly_test.py
@@ -11,6 +11,7 @@ import copy
 import random
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+import sys
 
 # For easier plots
 import plotly.express as px
@@ -29,6 +30,10 @@ from pyabc.transition.multivariatenormal import MultivariateNormalTransition  # 
 import logging
 logging.getLogger("pyopencl").setLevel(logging.ERROR)
 
+# Import arbitrary distribution class
+sys.path.append("C:/Users/gy17m2a/OneDrive - University of Leeds/Project/RAMP-UA/experiments/") 
+from ArbitraryDistribution import ArbitraryDistribution
+
 # RAMP model
 from microsim.initialisation_cache import InitialisationCache
 #from microsim.opencl.ramp.run import run_headless
@@ -46,7 +51,6 @@ from opencl_runner import OpenCLWrapper # Some additional functions to simplify 
 # Set this to False to recalculate all results (good on HPC or whatever).
 # If true then it loads pre-calculated results from pickle files (much quicker)
 LOAD_PICKLES = True
-
 
 ##########################################################################
 ##########################################################################
@@ -155,7 +159,7 @@ decorated_rvs = { name: pyabc.LowerBoundDecorator(rv, 0.0) for name, rv in all_r
 
 # The distribution is a collection of the priors and their name (e.g. 'asymptomatic'=asymptomatic_rv, 'work'=work_rv, .. )
 # so can just unpack the decorated_rvs dictionary and pass it straight in
-priors = pyabc.Distribution(**decorated_rvs)
+original_priors = pyabc.Distribution(**decorated_rvs)
 
 ##########################################################################
 ##########################################################################
@@ -166,114 +170,206 @@ priors = pyabc.Distribution(**decorated_rvs)
 # variables to be passed. I followed the suggestions from here: https://github.com/ICB-DCM/pyABC/issues/446
 ##########################################################################
 ##########################################################################
+# parameters_file = os.path.join("../../", "model_parameters/", "default.yml")  # Need to tell it where the default parameters are
+
+# da_window_size = 14 # Set the size of a data assimilation window in days:
+
+# admin_params = { "quiet":True, "use_gpu": True, "store_detailed_counts": True, "start_day": 0, "run_length": da_window_size,
+#                 "current_particle_pop_df": None,
+#                 "parameters_file": parameters_file, "snapshot_file": SNAPSHOT_FILEPATH, "opencl_dir": OPENCL_DIR
+#                }
+
+# template = OpenCLWrapper(const_params_dict, **admin_params)
+
+# # Not sure why this is needed. Wthout it we get an error when passing the template object to ABCSMC below
+# template.__name__ = OpenCLWrapper.__name__
+
+# print(f"Created a template model with: \n\tconstant params: {const_params_dict}\n\tadmin params:{admin_params}")
+
+
+##########################################################################
+# ##########################################################################
+# # First window
+# ##########################################################################
+# ##########################################################################
+# abc = pyabc.ABCSMC(
+#     models=template, # Model (could be a list)
+#     parameter_priors=priors, # Priors (could be a list)
+#     #summary_statistics=OpenCLWrapper.summary_stats,  # Summary statistics function (output passed to 'distance')
+#     distance_function=OpenCLWrapper.distance,  # Distance function
+#     sampler = pyabc.sampler.SingleCoreSampler()  # Single core because the model is parallelised anyway (and easier to debug)
+#     #sampler=pyabc.sampler.MulticoreEvalParallelSampler()  # The default sampler
+#     #transition=transition,  # Define how to transition from one population to the next
+#     )
+
+# # Observations are cases per msoa, but make it into a numpy array because this may be more efficient
+# # (first axis is the msoa number, second is the day)
+# observations = cases_msoa.iloc[:,0:405].to_numpy()
+
+# db_path = ("sqlite:///" + "ramp_da2.db")
+# run_id = abc.new(db_path, {'observation': observations, "individuals":individuals_df})
+
+# # Run :
+# history = abc.run(max_nr_populations=2)
+
+# print(f"Running new ABC with id {run_id}.... ", flush=True)
+
+# ##########################################################################
+# ##########################################################################
+# # Now try running again, using the posterior as a new prior
+# ##########################################################################
+# ##########################################################################
+# # Create the new prior from the posterio from window one
+# new_priors = ArbitraryDistribution(abc_history)
+
+# # Edit the da_window size in the admin params
+# admin_params['run_length'] = 28
+
+# # Recreate the template
+# template = OpenCLWrapper(const_params_dict, **admin_params)
+
+# # Not sure why this is needed. Wthout it we get an error when passing the template object to ABCSMC below
+# template.__name__ = OpenCLWrapper.__name__
+
+# # Prepare the ABC model
+# abc = pyabc.ABCSMC(
+#     models=template, # Model (could be a list)
+#     parameter_priors=new_priors, # Priors (could be a list)
+#     #summary_statistics=OpenCLWrapper.summary_stats,  # Summary statistics function (output passed to 'distance')
+#     distance_function=OpenCLWrapper.distance,  # Distance function
+#     sampler = pyabc.sampler.SingleCoreSampler()  # Single core because the model is parallelised anyway (and easier to debug)
+#     #sampler=pyabc.sampler.MulticoreEvalParallelSampler()  # The default sampler
+#     #transition=transition,  # Define how to transition from one population to the next
+#     )
+
+# # The results are stored in a database. We use a simple file database (sqlite) that creates a database
+# # file in the current directory
+# db_path = ("sqlite:///" + "ramp_da2.db")
+
+# # Each time you run it you get a new 'ID' (useful if you want to look up runs in the database)
+# # Note that this is where we give it the observations as well. Our distance function is expecting
+# # a dictionary with an 'observation' key in it, so we create a dictionary on the fly with { .. }
+# run_id = abc.new(db_path, {'observation': observations, "individuals":individuals_df})
+# print(f"Running new ABC with id {run_id}.... ", flush=True)
+
+# # Run the algorithm! (you can look up the max_nr_populations and minimum_epsilon arguments).
+# history = abc.run(max_nr_populations=2)
+
+# # Re-do the Algorithm diagnostics. I copied most of this from the docs
+# _, arr_ax = plt.subplots(2, 2)
+# pyabc.visualization.plot_sample_numbers(history, ax=arr_ax[0][0])
+# pyabc.visualization.plot_epsilons(history, ax=arr_ax[0][1])
+# pyabc.visualization.plot_effective_sample_sizes(history, ax=arr_ax[1][1])
+
+# plt.gcf().set_size_inches((12, 8))
+# plt.gcf().tight_layout()
+# plt.show()
+
+##########################################################################
+##########################################################################
+# Setup loop for running model
+##########################################################################
+##########################################################################
+# Path to parameters
 parameters_file = os.path.join("../../", "model_parameters/", "default.yml")  # Need to tell it where the default parameters are
-
-da_window_size = 14 # Set the size of a data assimilation window in days:
-
+# Set the size of a data assimilation window in days:
+da_window_size = 14 
+# Dictionary with parameters for running model
 admin_params = { "quiet":True, "use_gpu": True, "store_detailed_counts": True, "start_day": 0, "run_length": da_window_size,
                 "current_particle_pop_df": None,
-                "parameters_file": parameters_file, "snapshot_file": SNAPSHOT_FILEPATH, "opencl_dir": OPENCL_DIR
-               }
+                "parameters_file": parameters_file, "snapshot_file": SNAPSHOT_FILEPATH, "opencl_dir": OPENCL_DIR}
 
-template = OpenCLWrapper(const_params_dict, **admin_params)
+windows =10
+for window_number in range(3,windows):
+    print ("Window number: ", window_number)
+    print("Running for {} days".format(da_window_size * window_number))
+    
+    # Edit the da_window size in the admin params
+    admin_params['run_length'] = admin_params['run_length'] * window_number
 
-# Not sure why this is needed. Wthout it we get an error when passing the template object to ABCSMC below
-template.__name__ = OpenCLWrapper.__name__
-
-print(f"Created a template model with: \n\tconstant params: {const_params_dict}\n\tadmin params:{admin_params}")
-
-
-##########################################################################
-##########################################################################
-# First window
-##########################################################################
-##########################################################################
-abc = pyabc.ABCSMC(
-    models=template, # Model (could be a list)
-    parameter_priors=priors, # Priors (could be a list)
-    summary_statistics=OpenCLWrapper.summary_stats,  # Summary statistics function (output passed to 'distance')
-    distance_function=OpenCLWrapper.distance,  # Distance function
-    sampler = pyabc.sampler.SingleCoreSampler()  # Single core because the model is parallelised anyway (and easier to debug)
-    #sampler=pyabc.sampler.MulticoreEvalParallelSampler()  # The default sampler
-    #transition=transition,  # Define how to transition from one population to the next
-    )
-
-# Observations are cases per msoa, but make it into a numpy array because this may be more efficient
-# (first axis is the msoa number, second is the day)
-observations = cases_msoa.iloc[:,0:405].to_numpy()
-
-db_path = ("sqlite:///" + "ramp_da2.db")
-run_id = abc.new(db_path, {'observation': observations, "individuals":individuals_df})
-
-# Run :
-print('here')
-history = abc.run(max_nr_populations=2)
-
-print(f"Running new ABC with id {run_id}.... ", flush=True)
-history
-
-##########################################################################
-##########################################################################
-### Algorithm diagnostics
-##########################################################################
-##########################################################################
-
-_, arr_ax = plt.subplots(2, 2)
-
-pyabc.visualization.plot_sample_numbers(history, ax=arr_ax[0][0])
-pyabc.visualization.plot_epsilons(history, ax=arr_ax[0][1])
-#pyabc.visualization.plot_credible_intervals(
-#    history, levels=[0.95, 0.9, 0.5], ts=[0, 1, 2, 3, 4],
-#    show_mean=True, show_kde_max_1d=True,
-#    refval={'mean': 2.5},
-#    arr_ax=arr_ax[1][0])
-pyabc.visualization.plot_effective_sample_sizes(history, ax=arr_ax[1][1])
-
-plt.gcf().set_size_inches((12, 8))
-plt.gcf().tight_layout()
-
-#### Algorithm diagnostics
-_, arr_ax = plt.subplots(2, 2)
-
-pyabc.visualization.plot_sample_numbers(history, ax=arr_ax[0][0])
-pyabc.visualization.plot_epsilons(history, ax=arr_ax[0][1])
-#pyabc.visualization.plot_credible_intervals(
-#    history, levels=[0.95, 0.9, 0.5], ts=[0, 1, 2, 3, 4],
-#    show_mean=True, show_kde_max_1d=True,
-#    refval={'mean': 2.5},
-#    arr_ax=arr_ax[1][0])
-pyabc.visualization.plot_effective_sample_sizes(history, ax=arr_ax[1][1])
-
-plt.gcf().set_size_inches((12, 8))
-plt.gcf().tight_layout()
-
-########## Plot the marginal posteriors
-fig, axes = plt.subplots(3,int(len(priors)/2), figsize=(12,10))
-
-#cmap = { 0:'k',1:'b',2:'y',3:'g',4:'r' }  # Do this automatically for len(params)
-
-for i, param in enumerate(priors.keys()):
-    ax = axes.flat[i]
-    for t in range(history.max_t + 1):
-        df, w = history.get_distribution(m=0, t=t)
-        pyabc.visualization.plot_kde_1d(df, w, x=param, ax=ax,
-            label=f"{param} PDF t={t}",
-            alpha=1.0 if t==0 else float(t)/history.max_t, # Make earlier populations transparent
-            color= "black" if t==history.max_t else None # Make the last one black
+    # Template for model
+    template = OpenCLWrapper(const_params_dict, **admin_params)
+    # Not sure why this is needed. Wthout it we get an error when passing the template object to ABCSMC below
+    template.__name__ = OpenCLWrapper.__name__
+    
+    # Define priors
+    if window_number ==1:
+        priors = pyabc.Distribution(**decorated_rvs)
+    else:
+        priors = ArbitraryDistribution(abc_history)
+    
+    # set up model
+    abc = pyabc.ABCSMC(
+        models=template, # Model (could be a list)
+        parameter_priors=priors, # Priors (could be a list)
+        #summary_statistics=OpenCLWrapper.summary_stats,  # Summary statistics function (output passed to 'distance')
+        distance_function=OpenCLWrapper.distance,  # Distance function
+        sampler = pyabc.sampler.SingleCoreSampler()  # Single core because the model is parallelised anyway (and easier to debug)
+        #sampler=pyabc.sampler.MulticoreEvalParallelSampler()  # The default sampler
+        #transition=transition,  # Define how to transition from one population to the next
         )
-        if param!="work":
-            ax.set_xlim(0,1)
-        ax.legend(fontsize="small")
-        #ax.axvline(x=posterior_df.loc[1,param], color="grey", linestyle="dashed")
-        #ax.set_title(f"{param}: {posterior_df.loc[0,param]}")
-        ax.set_title(f"{param}")
-fig.tight_layout()
+    
+    # Observations are cases per msoa, but make it into a numpy array because this may be more efficient
+    # (first axis is the msoa number, second is the day)
+    observations = cases_msoa.iloc[:,0:405].to_numpy()
+    
+    db_path = ("sqlite:///" + "ramp_da2.db")
+    run_id = abc.new(db_path, {'observation': observations, "individuals":individuals_df})
+    
+    # Run :
+    abc_history = abc.run(max_nr_populations=2)
+    
+    # Save some info on the posterior parameter distributions.
+    _df, _w = abc.history.get_distribution(m=0, t=abc.history.max_t)
+    # Merge dataframe and weights and sort by weight (highest weight at the top)
+    _df['weight'] = _w
+    posterior_df = _df.sort_values('weight', ascending=False).reset_index()
+    posterior_df.to_csv("Plots/window_number{}_posterior_df.csv".format(window_number), index = False)
 
-### Analyse the posterior
-# Posterior distribution for the final population.
-# This is made up of the posterior estimates for each particle in the population and the associated weight.
-_df, _w = history.get_distribution(m=0, t=history.max_t)
-# Merge dataframe and weights and sort by weight (highest weight at the top)
-_df['weight'] = _w
-posterior_df = _df.sort_values('weight', ascending=False).reset_index()
-posterior_df
+    # ##########################################################################
+    # ##########################################################################
+    # ### Algorithm diagnostics
+    # ##########################################################################
+    # ##########################################################################
+    _, arr_ax = plt.subplots(2, 2)
+
+    pyabc.visualization.plot_sample_numbers(abc_history, ax=arr_ax[0][0])
+    pyabc.visualization.plot_epsilons(abc_history, ax=arr_ax[0][1])
+    #pyabc.visualization.plot_credible_intervals(
+    #    history, levels=[0.95, 0.9, 0.5], ts=[0, 1, 2, 3, 4],
+    #    show_mean=True, show_kde_max_1d=True,
+    #    refval={'mean': 2.5},
+    #    arr_ax=arr_ax[1][0])
+    pyabc.visualization.plot_effective_sample_sizes(abc_history, ax=arr_ax[1][1])
+
+    plt.gcf().set_size_inches((12, 8))
+    plt.gcf().tight_layout()
+    plt.savefig("Plots/window_number{}_algorithm_diagnostics.jpg".format(window_number))
+    plt.show()
+
+    # ########## Plot the marginal posteriors
+    fig, axes = plt.subplots(3,int(len(original_priors)/2), figsize=(12,10))
+
+    #cmap = { 0:'k',1:'b',2:'y',3:'g',4:'r' }  # Do this automatically for len(params)
+
+    for i, param in enumerate(original_priors.keys()):
+        ax = axes.flat[i]
+        for t in range(abc_history.max_t + 1):
+            df, w = abc_history.get_distribution(m=0, t=t)
+            pyabc.visualization.plot_kde_1d(df, w, x=param, ax=ax,
+                label=f"{param} PDF t={t}",
+                alpha=1.0 if t==0 else float(t)/abc_history.max_t, # Make earlier populations transparent
+                color= "black" if t==abc_history.max_t else None # Make the last one black
+            )
+            if param!="work":
+                ax.set_xlim(0,1)
+            ax.legend(fontsize="small")
+            #ax.axvline(x=posterior_df.loc[1,param], color="grey", linestyle="dashed")
+            #ax.set_title(f"{param}: {posterior_df.loc[0,param]}")
+            ax.set_title(f"{param}")
+    fig.tight_layout()
+    fig.show()
+    fig.savefig("Plots/window_number{}_marginal_posteriors.jpg".format(window_number))
+
+
+
