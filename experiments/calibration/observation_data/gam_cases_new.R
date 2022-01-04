5a66a53be5712dad1aa1910e65a999b67c3cf3fc
Molly <m.asher@hotmail.com>
2022-01-04 15:21:25 +0000

    smoothing obs

--- experiments/calibration/observation_data/gam_cases_new.R
+++ experiments/calibration/observation_data/gam_cases_new.R
@@ -1,19 +1,32 @@
+# Install libraries
 library(mgcv)
-setwd("C://Users/gy17m2a/OneDrive - University of Leeds/Project/RAMP-UA/experiments/calibration/observation_data")
 
+# Set working directory and import R file for downloading UK covid data
+setwd("C://Users/gy17m2a/OneDrive - University of Leeds/Project/RAMP-UA/experiments/calibration/observation_data")
 source("getUKCovidTimeSeries.R")
 
+
+#############################################################################################
+#############################################################################################
+#### Using old observations data
+#############################################################################################
+#############################################################################################
+# Get dataframe of Covid cases in the UK
 df <- getUKCovidTimeseries()
 
+# Filter this to just the Devon cases
 devon_cases <- df$tidyEnglandUnitAuth %>% 
   filter(CTYUA19NM == "Devon" & date <= as.Date("2020-06-24"))
 
 # Changing the value for row 84?
 devon_cases$cumulative_cases[84] <- 812
-# diff = subtracts first value from second value??
+# Gets new cases by comparing the cumulative cases on each date
 devon_cases$new_cases <- diff(c(0,(devon_cases$cumulative_cases)))
 
+# convert to numeric
 devon_cases$devon_date <- as.numeric(as.Date(devon_cases$date))
+
+# convert to dataframe
 devon_cases <- as.data.frame(devon_cases)
 
 # s is defning smooths in gam formula. Devon date is the variable to smooth as a function of
@@ -24,8 +37,8 @@ points(round(fitted.values(gam_Devon)*20), col = "red")
 abline(v = 38, lty = "dashed")
 gam_cases <- round(fitted.values(gam_Devon)*20)
 
+# save to file
 write.csv(gam_cases, "gam_cases.csv")
-
 
 #############################################################################################
 #############################################################################################
@@ -35,18 +48,25 @@ write.csv(gam_cases, "gam_cases.csv")
 # Read in case data for all MSOAs in Devon
 devon_cases_msoa <- read.csv("devon_daily_cases_t.csv")
 
+# Create an empty matrix to store the results for each MSOA in Devon
 devon_gam_cases_msoa <- data.frame(matrix(NA, nrow = 405, ncol = 0))
-multiplier = 1
+
+# define the multiplier (this is 20 above?)
+multiplier = 20
+
+# Create iterator 
 iter_i = 1
-# For each column/MSOA
-for (col in colnames(devon_cases_msoa)[1:length(colnames(devon_cases_msoa))-1]){
-  print(col)
+
+# For each MSOA, create a dataframe containing the cases on each day for that MSOA
+for (msoa_code in colnames(devon_cases_msoa)[1:length(colnames(devon_cases_msoa))-1]){
+  print(msoa_code)
   # select just one MSOA
-  one_msoa <- devon_cases_msoa[c("Day",col)]
+  one_msoa <- devon_cases_msoa[c("Day",msoa_code)]
   # rename column from MSOA name to cases
   colnames(one_msoa)[2] <- "PosCases"
-  # fit 
+  # Fit 
   gam_Devon <- mgcv::gam(PosCases ~ s(Day, bs = "cr"), data = one_msoa, family = nb())
+  # Plot
   plot(one_msoa$PosCases*multiplier,  ylab="Cases", xlab = "Day")
   points(round(fitted.values(gam_Devon)*multiplier), col = "red")
   abline(v = 38, lty = "dashed")
@@ -56,9 +76,11 @@ for (col in colnames(devon_cases_msoa)[1:length(colnames(devon_cases_msoa))-1]){
   one_msoa$gam_cases <- gam_cases
   #
   devon_gam_cases_msoa$col <-gam_cases
+  
   # rename
   colnames(devon_gam_cases_msoa)[iter_i] <- col
   
+  # Increase the iterator
   iter_i = iter_i+1
   }
 
