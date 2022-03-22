ec3fcc23d137077cb089263d84d1c30f956c5891
Molly <m.asher@hotmail.com>
2022-03-22 15:22:59 +0000

    removing files unneeded for this project (will still be available in other branch)

--- experiments/calibration/convert_nb.sh
+++
@@ -1,6 +0,0 @@
-jupyter nbconvert --to script $1
-echo "Script $1 converted"
-# These lines run the script (I've turned this off for now)
-# (although it wont work because '.ipynb' needs to be stripped from $1)
-#echo "Running $1.py "
-#nohup python $1.py &
--- experiments/calibration/ipynb2py.sh
+++
@@ -1,3 +0,0 @@
-# USAGE ipynb2py.sh [notebook.ipynb]
-jupyter nbconvert --to python $1 
-
