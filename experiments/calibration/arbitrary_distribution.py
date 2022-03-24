d1df07b58b42abfcb7255f41d6ece5b3d5e3700a
Molly <m.asher@hotmail.com>
2022-03-24 10:59:09 +0000

    improved commenting

--- experiments/calibration/arbitrary_distribution.py
+++ experiments/calibration/arbitrary_distribution.py
@@ -14,17 +14,20 @@ from pyabc.random_variables import Distribution
 from pyabc.parameters import Parameter
 
 pyabc.settings.set_figure_params('pyabc')  # for beautified plots
-x=1
+
 class ArbitraryDistribution(Distribution):
     """
-    Define an arbitrary distribution. Used so that the posterior from an ABC run can be re-used
-    as the prior in a new run. Does this by taking the abc_history object (output from an
-    ABC run) and generating a MultivariateNormalTransition (KDE) that can be sampled
+    Takes an abc_history object (output from an ABC run), and finds the parameter values associated with the final population.
+    These are then used to create a distribution which can be sampled using a MultivariateNormalTransition (KDE) 
+    This is required so that the posterior from an ABC run can be re-used as the prior in a new run. 
     """
     def __init__(self, abc_hist: History):
-        # Get the dataframe of particles (parameter point estimates) and associated weights
+        
+        # Get the dataframe of particles (parameter point estimates) and associated weights from the abc_history object
         dist_df, dist_w = abc_hist.get_distribution(m=0, t=abc_hist.max_t)
         
+        # Drop current_risk_beta as we don't want to include this in the distribution for running the model
+        # when dynamically calibrating, as it's set as a constant
         if 'current_risk_beta' in dist_df.columns:
             dist_df = dist_df.drop(columns = 'current_risk_beta')
         
@@ -36,13 +39,16 @@ class ArbitraryDistribution(Distribution):
         self.abc_hist = abc_hist
 
     def display(self):
-        print("display")
+
         # Get the dataframe of particles (parameter point estimates) and associated weights
         dist_df, dist_w = self.abc_hist.get_distribution(m=0, t=self.abc_hist.max_t)
 
+        # Drop current_risk_beta as we don't want to include this in the distribution for running the model
+        # when dynamically calibrating, as it's set as a constant
         if 'current_risk_beta' in dist_df.columns:
             dist_df = dist_df.drop(columns = 'current_risk_beta')
-
+            
+        # plot
         fig, axes = plt.subplots(2, 4, figsize=(14, 6), sharex=True, sharey=True)
         x = np.linspace(-1, 2, 99)  # (specified so that we have some whole numbers)
         marker = itertools.cycle((',', '+', '.', 'o', '*'))
@@ -54,32 +60,29 @@ class ArbitraryDistribution(Distribution):
                 dist_df, dist_w,
                 xmin=0, xmax=1.5,
                 x=variable, ax=ax)
-            #ax.legend()
-            #fig.show()
             i=i+1
-        #ax.legend()
-        #fig.tight_layout()
         fig.suptitle("Priors")
         fig.show()
 
     def rvs(self) -> Parameter:
-        #print("rvs")
         """Sample from the joint distribution, returning a Parameter object.
-           Just calls rvs() on the underlying kde"""
+           Just calls rvs() on the underlying kde
+           
+           Not just if functionality to resample if <0 is required now the GreaterThanZeroParameterTransition is defined (below)
+           """
+        # Sample a parameter vector from the KDE
         val = self.kde.rvs()
+        # If any of the parameter values are negative, then resample them 
         while val['asymptomatic']<0 or val['secondary_school']<0 or val['primary_school']<0 or val['retail']<0 or val['presymptomatic']<0 or val['symptomatic']<0 or val['work']<0:
             val = self.kde.rvs()
-            #print('resampling')
         return val
 
     def pdf(self, x: Union[Parameter, pd.Series, pd.DataFrame]) -> Union[float, np.ndarray]:
-        #print("pdf")
         """Get probability density at point `x` (product of marginals).
         Just calls pdf(x) on the underlying kde"""
         return self.kde.pdf(x)
 
     def __repr__(self):
-        #print("_repr_")
         print(f"<ArbitraryDistribution\n    " + \
         ",\n    ".join(str(p) for p in self.kde.X.columns) + ">")
         return f"<ArbitraryDistribution\n    " + \
